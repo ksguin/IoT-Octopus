98afe2fb2807562ba138074f4870645d615a1343
Kumar Subhodeep Guin <kumar.subhodeep64@gmail.com>
2019-06-23 02:05:34 +0530

    Update BLE On-Connect Data Write
    When BLE is not connected to any device, it will not be taking sensor
    readings. Only on connecting to BLE, the sending process will resume.

--- Assignments/Task_3_Interface_BLE_ESP32_get_Sensor_Data/Task_3_Interface_BLE_ESP32_get_Sensor_Data.ino
+++ Assignments/Task_3_Interface_BLE_ESP32_get_Sensor_Data/Task_3_Interface_BLE_ESP32_get_Sensor_Data.ino
@@ -6,6 +6,17 @@
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 BLECharacteristic *pCharacteristic;
+bool deviceConnected = false;
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
 
 void setup() {
   
@@ -15,7 +26,8 @@ void setup() {
   BLEDevice::init("MyESP32");   //name of BLE device
   
   BLEServer *pServer = BLEDevice::createServer();   //ESP32 as BLE Server
-  
+  pServer->setCallbacks(new MyServerCallbacks());
+
   BLEService *pService = pServer->createService(SERVICE_UUID);    //Creating Service for the Server with UUID
   //defining characteristic of the Service(here READ only because the Client needs to only read sensor data)
   pCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID, 
@@ -32,8 +44,13 @@ void setup() {
 }
 
 void loop() {
-    pCharacteristic->setValue("1");
-    delay(4000);
-    pCharacteristic->setValue("3");
-    delay(3000);
+  if (deviceConnected) {
+    float txValue = random(1,9) / 3.14 ;
+    char txString[8];
+    dtostrf(txValue, 1, 2, txString); // float_val, min_width, digits_after_decimal, char_buffer
+    pCharacteristic->setValue(txString);
+    Serial.print("Data Written: ");
+    Serial.println(txString);
+  }
+  delay(3000);
 }
