0a432f8f187e0cc35aee93a9a26c097e6fa8024e
Kumar Subhodeep Guin <kumar.subhodeep64@gmail.com>
2019-06-25 20:57:40 +0530

    Initial commit Upload multi-fields on ThingSpeak
    Connecting ESP8266 to WiFi and sending multiple data to ThingSpeak in one transaction.

---
+++ Assignments/Task_5_Upload_DHT11_ThingSpeak/Task_5_Upload_DHT11_ThingSpeak.ino
@@ -0,0 +1,80 @@
+#include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <ThingSpeak.h>
+#include <WiFiClient.h>
+
+#define DHTPIN 2  //GPIO2, i.e. D4 on ESP8266
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+const char* ssid = "IIESTS";
+const char* password = "";
+
+//ThingSpeak credentials
+unsigned long myChannelNumber = ; //Your Channel Number (Without Brackets)
+const char * myWriteAPIKey = ""; //Your Write API Key
+
+WiFiClient client;
+
+#define TempField 1
+#define HumidityField 2
+/* ------------------------------------------------------------------------ */
+
+int  prevtemp=0;
+int  prevhum=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  Serial.begin(9600);// speed of ESP8266
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  //LED OFF when WiFi isn't connected
+  digitalWrite(LED_BUILTIN, HIGH);  //Built-in LED works opposite
+
+  WiFi.begin(ssid,password);
+  Serial.println();
+  Serial.print("WiFi connecting to ");
+  Serial.println(ssid);
+
+  //connecting dots
+  Serial.print("Connecting");
+  while( WiFi.status() != WL_CONNECTED ){
+      delay(500);
+      Serial.print(".");
+  }
+
+  //LED ON when WiFi connection established
+  digitalWrite(LED_BUILTIN, LOW);
+
+  Serial.println();
+  Serial.print("Connection Established! ");
+  Serial.print("NodeMCU IP Address : ");
+  Serial.println(WiFi.localIP());
+
+  ThingSpeak.begin(client);  // Initialize ThingSpeak with client
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float temp_data = random(20,50);
+  float humidity_data = random(20,90);
+
+  // set the fields with the values
+  ThingSpeak.setField(TempField, temp_data);
+  ThingSpeak.setField(HumidityField, humidity_data);
+  // writing multiple values to the ThingSpeak channel in one transaction
+  int status = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  //checking if data was successfully written.
+  if(status == 200){
+    Serial.print("Channel Update success:- Temp:");
+    Serial.print(temp_data);
+    Serial.print(" Humidity: ");
+    Serial.println(humidity_data);
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(status));
+  }
+  
+  delay(16000); // ThingSpeak will only accept updates atleast after every 15 seconds.
+}
